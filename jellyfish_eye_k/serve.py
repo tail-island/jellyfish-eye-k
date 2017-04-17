1d64e7fe87fb4ff97d9c3752ebecfcb9ef7e8eab
OJIMA Ryoji <rojima1@gmail.com>
2017-04-17 18:53:54 +0900

    ...

--- jellyfish_eye_k/serve.py
+++ jellyfish_eye_k/serve.py
@@ -12,15 +12,12 @@ def classify(x):
 
 
 if __name__ == '__main__':
-    import matplotlib.animation as animation
     import matplotlib.pyplot as plot
     import time
     
     from jellyfish_eye_k.data_set import load_data
 
     
-    CLASS_CAPTIONS = ["Calendar", "Drink", "Tissue Paper"]
-
     _, _, (x_test, y_test) = load_data()
 
     y_pred = []
@@ -30,25 +27,12 @@ if __name__ == '__main__':
         y_pred.append(classify(x))
     finishing_time = time.time()
 
-    accuracy = sum(1 if y_test == y_pred else 0 for y_test, y_pred in zip(y_test, y_pred)) / len(y_pred)
-    
     print('Elapsed time: {0:.4f} sec'.format((finishing_time - starting_time) / len(y_pred)))
-    print('Accuracy = {0:.4f}'.format(accuracy))
+    print('Accuracy = {0:.4f}'.format(sum(1 if y_test == y_pred else 0 for y_test, y_pred in zip(y_test, y_pred)) / len(y_pred)))
 
-    figure = plot.figure()
-    images = []
-
-    plot.imshow([[[0.0, 0.0, 0.0, 1.0] for x in range(100)] for y in range(100)])
-    
     for x, y_true, y_pred in zip(x_test, y_test, y_pred):
-        images.append([plot.imshow(x),
-                       plot.text(50, 10, 'Label: {0}'.format(CLASS_CAPTIONS[y_true]), color='w', horizontalalignment='center', size=20),
-                       plot.text(50, 15, 'Answer: {0}'.format(CLASS_CAPTIONS[y_pred]), color='w', horizontalalignment='center', size=20),
-                       plot.text(50, 25, 'Correct!' if y_true == y_pred else 'Wrong...', color='c' if y_true == y_pred else 'm', horizontalalignment='center', size=20)])
+        print('{0} : {1}'.format(y_true, y_pred))
 
-    images.append([plot.text(50, 50, 'Accuracy = {0:.4f}'.format(accuracy), color='w', horizontalalignment='center', size=20)])
-
-    artist_animation = animation.ArtistAnimation(figure, images, interval=500, repeat=False)
-
-    plot.get_current_fig_manager().full_screen_toggle()
-    plot.show()
+        plot.imshow([[[0.0, 0.0, 1.0, 1.0] for x in range(100)] for y in range(100)])
+        plot.imshow(x)
+        plot.show()
